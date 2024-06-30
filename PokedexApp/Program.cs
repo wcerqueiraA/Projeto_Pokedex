8cce1e1f1fcbffc96faa7209c65a0ea92aea2604
Wellington <wellington.cerqueira32@gmail.com>
2024-06-30 17:18:30 -0300

    Configurado o HttpClient

--- PokedexApp/Program.cs
+++ PokedexApp/Program.cs
@@ -1,7 +1,11 @@
+using PokedexApp.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+builder.Services.AddHttpClient<PokeApiService>();
 
 
 var app = builder.Build();
